b190077121da8cc66ee6705f9fc327872d50a0c2
TomaszBury <tomasz.bury+github@gmail.com>
2018-03-06 20:05:14 +0100

    Spring5 Web App

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -35,36 +35,29 @@ public class Author {
         return id;
     }
 
-
     public void setId(Long id) {
         this.id = id;
     }
-
 
     public String getFirstName() {
         return firstName;
     }
 
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
 
     public String getLastName() {
         return lastName;
     }
 
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-
     public Set<Book> getBooks() {
         return books;
     }
-
 
     public void setBooks(Set<Book> books) {
         this.books = books;
