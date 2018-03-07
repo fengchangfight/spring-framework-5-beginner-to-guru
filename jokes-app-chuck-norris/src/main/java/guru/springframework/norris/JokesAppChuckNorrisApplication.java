36c196b8bb3244e362be59ae095b2b5307b940e9
TomaszBury <tomasz.bury+github@gmail.com>
2018-03-07 13:01:23 +0100

    Chuck Norris Joke

---
+++ jokes-app-chuck-norris/src/main/java/guru/springframework/norris/JokesAppChuckNorrisApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.norris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAppChuckNorrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesAppChuckNorrisApplication.class, args);
+	}
+}
---
+++ jokes-app-chuck-norris/src/main/java/guru/springframework/norris/services/JokeService.java
@@ -0,0 +1,5 @@
+package guru.springframework.norris.services;
+
+public interface JokeService {
+    String getJoke();
+}
