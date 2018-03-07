aea9f399eb406089135cfacabe8a8611fcc1bd6a
TomaszBury <tomasz.bury+github@gmail.com>
2018-03-07 09:43:11 +0100

    Spring Bean Life Cycle

--- di-demo/src/main/java/guru/springframework/didemo/services/PrimarySpanishGreetingService.java
+++ di-demo/src/main/java/guru/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -8,6 +8,13 @@ import org.springframework.stereotype.Service;
 @Profile("es")
 @Primary
 public class PrimarySpanishGreetingService implements GreetingService {
+
+    private GreetingRepository greetingRepository;
+
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return "Servicio de saludo Primario.";
